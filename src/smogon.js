cc964af59c5bd874965d585de1320bf5a4320146
te1 <te1@users.noreply.github.com>
2019-12-13 23:33:26 +0100

    smogon pokemon

--- src/smogon.js
+++ src/smogon.js
@@ -11,6 +11,24 @@ function getTypeName(id) {
   return getName(type.name);
 }
 
+function getAbilityName(id) {
+  let ability = all.abilities[id];
+
+  return getName(ability.name);
+}
+
+function getMoveName(id) {
+  let move = all.moves[id];
+
+  return getName(move.name);
+}
+
+function getSpeciesName(id) {
+  let species = all.species[id];
+
+  return getName(species.name);
+}
+
 async function exportAbilities(target, data) {
   console.log(`processing ${data.length} abilities...`);
 
@@ -95,6 +113,98 @@ async function exportMoves(target, data) {
   }
 }
 
+async function exportPokemon(target, data) {
+  console.log(`processing ${data.length} pokemon...`);
+
+  let index = [];
+  let details = [];
+  let obj,
+    isBattleOnly,
+    abilities,
+    evos,
+    prevo,
+    altBattleFormes,
+    learnset,
+    result;
+
+  _.forEach(data, (pkmn, id) => {
+    if (!pkmn || pkmn.isNonstandard) {
+      return;
+    }
+
+    isBattleOnly = pkmn.isBattleOnly || undefined;
+
+    obj = {
+      id,
+      name: getName(pkmn.name),
+      caption: pkmn.name,
+      types: _.map(pkmn.types, getTypeName),
+      num: pkmn.num,
+      isBattleOnly,
+    };
+
+    index.push(obj);
+
+    abilities = {};
+    if (pkmn.abilities[0]) {
+      abilities['1'] = getAbilityName(pkmn.abilities[0]);
+    }
+    if (pkmn.abilities[1]) {
+      abilities['2'] = getAbilityName(pkmn.abilities[1]);
+    }
+    if (pkmn.abilities[2]) {
+      abilities['H'] = getAbilityName(pkmn.abilities[2]);
+    }
+
+    evos = pkmn.evos.length ? _.map(pkmn.evos, getSpeciesName) : undefined;
+    prevo = pkmn.prevo != null ? getSpeciesName(pkmn.prevo) : undefined;
+
+    altBattleFormes = pkmn.altBattleFormes.length
+      ? _.map(pkmn.altBattleFormes, getSpeciesName)
+      : undefined;
+
+    learnset = _.map(pkmn.learnset, entry => {
+      result = {};
+
+      result.how = _.filter(entry.how, x => _.startsWith(x, '8'));
+
+      if (!result.how.length) {
+        return;
+      }
+
+      result.what = getMoveName(entry.what);
+
+      return result;
+    });
+    learnset = _.reject(learnset, x => x == null);
+
+    details.push({
+      ...obj,
+      altBattleFormes,
+      desc: pkmn.desc,
+      shortDesc: pkmn.shortDesc,
+      baseStats: pkmn.baseStats,
+      abilities,
+      evos,
+      prevo,
+      height: pkmn.heightm,
+      weight: pkmn.weightkg,
+      learnset,
+    });
+  });
+
+  index = _.orderBy(index, 'num');
+  details = _.orderBy(details, 'num');
+
+  console.log(`writing ${index.length} pokemon...`);
+  await exportData(path.join(target, 'pokemon/index.json'), index);
+
+  console.log(`writing ${details.length} pokemon details...\n`);
+  for (const pkmn of details) {
+    await exportData(path.join(target, 'pokemon', pkmn.name + '.json'), pkmn);
+  }
+}
+
 async function exportItems(target, data) {
   console.log(`processing ${data.length} items...`);
 
@@ -137,6 +247,7 @@ async function exportAll(target) {
 
   await exportAbilities(target, all.abilities);
   await exportMoves(target, all.moves);
+  await exportPokemon(target, all.species);
   await exportItems(target, all.items);
 
   console.log('done\n');

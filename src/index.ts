154f40b096cee0e56f32e32d49f11908e68fb602
te <te1@users.noreply.github.com>
2022-08-06 10:45:40 +0200

    move type export to own file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import * as fs from 'fs-extra';
-import * as path from 'path';
-import * as _ from 'lodash';
 import { Dex } from '@pkmn/dex';
-import { Generation, Generations, GenerationNum, TypeName } from '@pkmn/data';
-import { exportData } from './utils';
+import { Generations, GenerationNum } from '@pkmn/data';
+import { exportTypes } from './type';
 
 const target = './generated/';
 
 const gens = new Generations(Dex);
 const genNums: GenerationNum[] = [1, 2, 3, 4, 5, 6, 7, 8];
-
-main();
 
 async function main() {
   try {
@@ -22,7 +18,6 @@ async function main() {
       console.log(`*** gen ${genNum} ***`);
 
       await exportTypes(gen, target);
-      console.log('');
 
       console.log('');
     }
@@ -33,57 +28,4 @@ async function main() {
   }
 }
 
-async function exportTypes(gen: Generation, target: string) {
-  console.log('- types');
-
-  process.stdout.write('\t' + 'loading data...');
-  const data = await fs.readJson(`./data/type.json`);
-
-  let result = [];
-
-  for (const type of gen.types) {
-    if (type.name === '???') {
-      continue;
-    }
-
-    let damageTakenList = [];
-    let damageDoneList = [];
-
-    for (const otherType of gen.types) {
-      if (otherType.name === '???') {
-        continue;
-      }
-
-      damageTakenList.push({
-        type: otherType.id,
-        value: otherType.totalEffectiveness(type.name),
-      });
-
-      damageDoneList.push({
-        type: otherType.id,
-        value: type.totalEffectiveness(otherType.name),
-      });
-    }
-
-    damageTakenList = _.orderBy(damageTakenList, ['value', 'type']);
-    const damageTakenMap = _.keyBy(damageTakenList, 'type');
-    const damageTakenFlat = _.mapValues(damageTakenMap, 'value');
-
-    damageDoneList = _.orderBy(damageDoneList, ['value', 'type']);
-    const damageDoneMap = _.keyBy(damageDoneList, 'type');
-    const damageDoneFlat = _.mapValues(damageDoneMap, 'value');
-
-    result.push({
-      slug: type.id,
-      name: type.name,
-      color: data[type.id]?.color,
-      damageTaken: damageTakenFlat,
-      damageDone: damageDoneFlat,
-    });
-  }
-
-  result = _.sortBy(result, 'slug');
-
-  process.stdout.write('\t' + `writing ${result.length} types...`);
-  await exportData(path.join(target, String(gen.num), 'type.json'), result);
-}
+main();
---
+++ src/type.ts
@@ -0,0 +1,74 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import * as _ from 'lodash';
+import { Generation, Type, Types } from '@pkmn/data';
+import { exportData } from './utils';
+
+export async function exportTypes(gen: Generation, target: string) {
+  console.log('- types');
+
+  console.log('\t' + 'loading data...');
+  const data = await fs.readJson(`./data/type.json`);
+
+  let result = [];
+
+  for (const type of gen.types) {
+    if (shouldSkipType(type)) {
+      continue;
+    }
+
+    const [damageTaken, damageDone] = getDamage(type, gen.types);
+
+    result.push({
+      slug: type.id,
+      name: type.name,
+      color: data[type.id]?.color,
+      damageTaken,
+      damageDone,
+    });
+  }
+
+  result = _.sortBy(result, 'slug');
+
+  console.log('\t' + `writing ${result.length} types...`);
+  await exportData(path.join(target, `gen${gen.num}`, 'type.json'), result);
+}
+
+function shouldSkipType(type: Type) {
+  if (type.name === '???') {
+    return true;
+  }
+
+  return false;
+}
+
+function getDamage(type: Type, allTypes: Types) {
+  let takenList = [];
+  let doneList = [];
+
+  for (const otherType of allTypes) {
+    if (shouldSkipType(otherType)) {
+      continue;
+    }
+
+    takenList.push({
+      type: otherType.id,
+      value: otherType.totalEffectiveness(type.name),
+    });
+
+    doneList.push({
+      type: otherType.id,
+      value: type.totalEffectiveness(otherType.name),
+    });
+  }
+
+  takenList = _.orderBy(takenList, ['value', 'type']);
+  const takenMap = _.keyBy(takenList, 'type');
+  const taken = _.mapValues(takenMap, 'value');
+
+  doneList = _.orderBy(doneList, ['value', 'type']);
+  const doneMap = _.keyBy(doneList, 'type');
+  const done = _.mapValues(doneMap, 'value');
+
+  return [taken, done];
+}
